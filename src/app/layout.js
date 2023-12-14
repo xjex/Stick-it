4e895c1200598475a32f9edde344cb7c20756cc7
xjex <dev.xjmanaloto@gmail.com>
2023-12-14 17:04:03 +0800

    Data for SEO

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,6 +8,14 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Stick-up",
   description: "A freedom wall for everyone.",
+  image:
+    "https://xleahmgrjfdmuxgfgcog.supabase.co/storage/v1/object/public/sticky/meta/Stickup.PNG?t=2023-12-14T08%3A39%3A26.489Z",
+  url: "https://stick-up.vercel.app/",
+  keywords:
+    "social media, sticky notes, stick-up, stick, up, stickup, stick-up!, stick-up!, stickup!, stick up, noteswall, wall, post, free, posting, freedom, freedom wall, freedomwall, freedom-wall, freedom_wall, freedomwall,",
+
+  author: "Xavier Joseph Manaloto",
+  robots: "index, follow",
 };
 
 export default function RootLayout({ children }) {
